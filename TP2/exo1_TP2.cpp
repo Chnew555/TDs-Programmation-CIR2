66018e59cf0fddaff684c0deb9dcc2700c0f0fc3
Noa Rintharamy <nonorintha@gmail.com>
2025-09-15 11:10:27 +0200

    Exo1 Fait

---
+++ TP2/exo1_TP2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define multiplier(x, y) ((x) * (y))
+
+int main(void)
+{
+    int a;
+    int b;
+    cout << "Entrez une valeur :" << endl;
+    cin >> a;
+    cout << "Entrez une autre valeur :" << endl;
+    cin >> b;
+    cout << multiplier(a + 1, b + 1) << endl;
+}
